72f5f5cc0650761161b2dc14c395d43f62ca37ed
Anderson Silva <andssilva.23@gmail.com>
2021-12-15 12:57:39 -0400

    feat: :rocket: cria a página principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { GetStaticProps } from 'next';
-
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +27,67 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | Spacetraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main>
+        <div>
+          <a href="http://google.com">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <time>
+              <FiCalendar />
+              15 Mar 2021
+            </time>
+            <span>
+              <FiUser />
+              Joseph Oliveira
+            </span>
+          </a>
+          <a href="http://google.com">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <time>
+              <FiCalendar />
+              15 Mar 2021
+            </time>
+            <span>
+              <FiUser />
+              Joseph Oliveira
+            </span>
+          </a>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+/* export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 20,
+    }
+  );
+
+  console.log(postsResponse);
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      subtitle: post.data
+    }
+  })
+
+  return {
+    props: {},
+  };
+}; */
